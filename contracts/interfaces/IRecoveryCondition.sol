f654e27a42cc71bf9d2176543d84f502e300ffb6
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2025-07-19 12:20:55 +0530

    updated recovery condition contract interface

--- contracts/interfaces/IRecoveryCondition.sol
+++ contracts/interfaces/IRecoveryCondition.sol
@@ -3,5 +3,6 @@ pragma solidity ^0.8.19;
 
 interface IRecoveryCondition {
     function isRecoverable(address contractAddress) external view returns (bool);
-    function triggerRecovery(address contractAddress) external;
+    function canTriggerRecovery(address contractAddress) external view returns (bool);
+    function triggerRecovery(address contractAddress, address newOwner) external;
 }
